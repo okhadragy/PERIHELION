339b2e3b191ae080f02d0a57e0a337a6efbcc474
okhadragy <okhadragy@gmail.com>
2024-10-06 11:53:30 +0300

    final change

--- src/script.js
+++ src/script.js
@@ -136,8 +136,17 @@ const song1 = document.getElementById('song1')
 const musicslider = document.getElementById('musicslider');
 const narrslider = document.getElementById('narrslider');
 const speedslider = document.getElementById('speedslider');
+const body = document.getElementsByTagName("body")[0]
 song1.volume = musicslider.value;
 
+function addFilters() {
+  const div = document.createElement("div");
+  div.style.display = "none";
+  div.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" style="display:none;"><filter id="protanopia-filter"><feColorMatrix type="matrix" values="0.567, 0.433, 0, 0, 0 0.558, 0.442, 0, 0, 0 0, 0.242, 0.758, 0, 0 0, 0, 0, 1, 0" /></filter><filter id="deuteranopia-filter"><feColorMatrix type="matrix" values="0.625, 0.375, 0, 0, 0 0.7, 0.3, 0, 0, 0 0, 0.3, 0.7, 0, 0 0, 0, 0, 1, 0" /></filter><filter id="tritanopia-filter"><feColorMatrix type="matrix" values="0.95, 0.05, 0, 0, 0 0, 0.433, 0.567, 0, 0 0, 0.475, 0.525, 0, 0 0, 0, 0, 1, 0" /></filter></svg>';
+  body.appendChild(div);
+}
+
+addFilters()
 //
 loadingManager.onLoad = function () {
 
